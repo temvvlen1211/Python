6dfccbd90f9ba1a1fc355263afcebd6b109fae58
Your Nameclear <you@example.com>
2023-08-21 18:13:27 +0800

    python loop array totural

--- script.py
+++ script.py
@@ -1,2 +1,31 @@
-my_name = "Temuulen"
-print("Hello and welcome " + my_name + "!")
+# my_name = "Temuulen"
+# print("Hello and welcome " + my_name + "!")
+# gg = input("what is your name?")
+# print("hello" + gg)
+# birth_year = input("Enter your birth year:")
+# age = 2023 - int(birth_year)
+# # print("birth_year", age)
+# course = "Python for beginners"
+# print(course.replace("g", "5"))
+# temprature = 25
+# if temprature >= 30:
+#     print("hot temprature")
+#     print("Drink plenty of water")
+# elif temprature > 20:
+#     print("it's a nice day")
+# else:
+    # print("cold temprature")
+# i =1 
+# while i <= 20:
+#     print(i * "*" )
+#     i+=1
+numbers = [1,2,3,4,5]
+
+for item in numbers:
+    print(item)
+    
+i=0 
+while i <len(numbers):
+    print(numbers[i])
+    i+=1
+     
