c23c60163c278e805ca12844fb3b51d529bbf48b
Your Nameclear <you@example.com>
2023-08-22 19:25:40 +0800

    selenium and testing

--- script.py
+++ script.py
@@ -14,18 +14,88 @@
 # elif temprature > 20:
 #     print("it's a nice day")
 # else:
-    # print("cold temprature")
-# i =1 
+# print("cold temprature")
+# i =1
 # while i <= 20:
 #     print(i * "*" )
 #     i+=1
-numbers = [1,2,3,4,5]
+# numbers = [1, 2, 3, 4, 5]
 
-for item in numbers:
-    print(item)
+# for item in numbers:
+#     print(item)
+
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i += 1
+
+
+# def func():
+#     print("gg")
+
+
+# func()
+
+# x = None
+# print(x)
+
+# import random
+
+# print(random.randrange(0, 10))
+
+# import camelcase
+
+# c = camelcase.CamelCase()
+
+# txt = "lorem ipsum dolor sit amet"
+
+# print(c.hump(txt))
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+
+
+# def test_eight_components():
+#     driver = webdriver.Chrome()
+
+#     driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+#     title = driver.title
+#     assert title == "Web form"
+
+#     driver.implicitly_wait(0.5)
+
+#     text_box = driver.find_element(by=By.NAME, value="my-text")
+#     submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
     
-i=0 
-while i <len(numbers):
-    print(numbers[i])
-    i+=1
-     
+#     text_box = driver.find_element(by=By.NAME, value="my-password")
+    
+#     submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+
+#     text_box.send_keys("Selenium")
+#     submit_button.click()
+
+#     message = driver.find_element(by=By.ID, value="message")
+#     value = message.text
+#     assert value == "Received!"
+    
+
+#     driver.quit()
+# test_eight_components()
+
+# from selenium import webdriver
+
+
+# driver = webdriver.Chrome()
+
+# driver.get("http://youtube.com")
+
+# driver.quit()
+ 
+ 
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""    
+print(a)
